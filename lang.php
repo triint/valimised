2fd25fee17ad908273774a7af2a23da6dda0766a
kpellis <kert.pellis@gmail.com>
2016-03-22 21:10:40 +0200

    .

--- lang.php
+++ lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$str_intro = array("et" => "Tere tulemast aasta 2016 e-valimiste kodulehele!", "en" => "Welcome");
+$str_intro = array("et" => "Tere tulemast aasta 2016 e-valimiste kodulehele!", "en" => "Welcome to the e-voting homepage of 2016!");
 
 ?>
