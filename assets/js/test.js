e14af9aa4c45fc3dbe2bec97cf36c9e5d559172a
kpellis <kpellis@users.noreply.github.com>
2016-05-22 07:31:30 +0300

    Add files via upload

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,22 +1,29 @@
-	function loadChart()
+var data;
+function loadChart(query)
+{
+	data=query;
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(rdy);
+}
+function rdy()
+{
+	drawChart(data);
+	//alert("a");
+}
+function drawChart(query) {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Party');
+    data.addColumn('number', 'Votes');
+	for(var i=0;i<query.length;i++)	
 	{
-		google.charts.load('current', {'packages':['corechart']});
-		google.charts.setOnLoadCallback(drawChart);
+		data.addRow([query[i]['Partei'],parseInt(query[i]['Votes'])]);
 	}
-      function drawChart(query) {
-		//alert(query[0]['Votes']);
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Party');
-        data.addColumn('number', 'Votes');
-		//data.addRow(['test1',1]);
-		//data.addRow(['test2',2]);
-		for(var i=0;i<query.length;i++)	
-		{
-			data.addRow([query[i]['Partei'],parseInt(query[i]['Votes'])]);
-		}
-        var options = {'title':'',
-                       'width':700,
-                       'height':450};
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+       var options = {'title':'',
+                      'width':700,
+                   'height':450};
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
+ }
+
+	
+     
