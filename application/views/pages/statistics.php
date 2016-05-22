1ab81283c4ae24d27ba62ae51c5b12986f61346f
kpellis <kpellis@users.noreply.github.com>
2016-05-22 07:30:44 +0300

    Add files via upload

--- application/views/pages/statistics.php
+++ application/views/pages/statistics.php
@@ -1,6 +1,6 @@
 <?php include "lang.php";?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script defer src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" async src="https://www.gstatic.com/charts/loader.js"></script>
 
 	<div id="content">
 		<a href="<?php echo base_url();?>index.php/statistics?type=all">Kogu riigis</a>&nbsp
@@ -11,18 +11,19 @@
 		<div id="contentrefr"></div>
 	</div>
 	<div id="chart_div"></div>
-<script type="text/javascript" src="../assets/js/test.js"></script>
-<script type="text/javascript">
-loadChart();
+	
+<script  type="text/javascript">
+window.onload = function(){
 $(document).ready (function () {
 	$.ajaxSetup ({
         cache: false
     });
-	$('#contentrefr').load ('/index.php/<?=$refr?>', 'update=true');
+	$('#contentrefr').load ('/index.php/<?=$refr?>?load=1', 'update=true');
     var updater = setInterval (function () {
-        $('div#contentrefr').load ('/index.php/<?=$refr?>', 'update=true');
+        $('div#contentrefr').load ('/index.php/<?=$refr?>?load=0', 'update=true');
     }, 5000);
 });
+}
 </script>
 
 
--- application/views/pages/statistics2.php
+++ application/views/pages/statistics2.php
@@ -1,7 +1,13 @@
 <?php include "lang.php";?>
-<!--Load the AJAX API-->
-
 <script type="text/javascript" src="../assets/js/test.js"></script>
+<?php if(isset($_GET['load'])): if($_GET['load']==1):?>	
+<script>
+loadChart(<?=json_encode($query);?>);
+</script>
+<?php else:?>
+
 <script>
 drawChart(<?=json_encode($query);?>);
 </script>
+<?php endif;?>
+<?php endif;?>
